5c952990a921e861a0bf13dac5c5688b5794b153
Fernando Nathaniel Sutanto <67040382+nandonathaniel@users.noreply.github.com>
2023-12-01 15:48:06 +0700

    Update search_letor.py

--- engine/search_letor.py
+++ engine/search_letor.py
@@ -1,57 +1,3 @@
-# from engine.bsbi import BSBIIndex
-# from engine.compression import VBEPostings
-# from engine.letor import LambdaMart
-# import time
-
-# class SearchLetor:
-#     def __init__(self):
-#         self.BSBI_instance = BSBIIndex(data_dir='engine/collections',
-#                                 postings_encoding=VBEPostings,
-#                                 output_dir='engine/index')
-#         self.BSBI_instance.load()
-#         self.letor = LambdaMart()
-    
-#     def ranking(self, query, k=10):
-#         print("Query  : ", query)
-#         print("Results:")
-
-#         start_time = time.time()
-#         tf_idf_result = self.BSBI_instance.retrieve_tfidf(query, k=50)
-#         reranked_with_letor = self.letor.rerank_letor(query, [t[1] for t in tf_idf_result])
-#         for (score, doc) in reranked_with_letor[:k]:
-#             print(f"{doc:30} {score:>.3f}")
-#         print()
-#         end_time = time.time()
-
-#         elapsed_time = end_time - start_time
-#         elapsed_time_ms = elapsed_time * 1000
-#         print(f"Elapsed time: {elapsed_time_ms:>.3f} ms")
-
-#     def rankingReturn(self, query, k=10):
-#         start_time = time.time()
-#         tf_idf_result = self.BSBI_instance.retrieve_tfidf(query, k=20)
-#         reranked_with_letor = self.letor.rerank_letor(query, [t[1] for t in tf_idf_result])
-
-#         results_list = []
-#         for (score, doc) in reranked_with_letor[:k]:
-#             results_list.append({"doc": doc, "score": score})
-
-#         end_time = time.time()
-#         elapsed_time = end_time - start_time
-#         elapsed_time_ms = elapsed_time * 1000
-
-#         elapsed_time_ms_formatted = "{:.3f}".format(elapsed_time_ms)
-
-#         return {"time": elapsed_time_ms_formatted, "results": results_list}
-
-
-
-# if __name__ == '__main__':
-#     sl = SearchLetor()
-#     while True:
-#         s = input()
-#         sl.ranking(s)
-
 from engine.bsbi import BSBIIndex
 from engine.compression import VBEPostings
 from engine.letor import LambdaMart
@@ -63,7 +9,7 @@ class SearchLetor:
                                 postings_encoding=VBEPostings,
                                 output_dir='engine/index')
         self.BSBI_instance.load()
-        # self.letor = LambdaMart()
+        self.letor = LambdaMart()
     
     def ranking(self, query, k=10):
         print("Query  : ", query)
@@ -83,8 +29,8 @@ class SearchLetor:
 
     def rankingReturn(self, query, k=10):
         start_time = time.time()
-        reranked_with_letor = self.BSBI_instance.retrieve_tfidf(query, k=20)
-        # reranked_with_letor = self.letor.rerank_letor(query, [t[1] for t in tf_idf_result])
+        tf_idf_result = self.BSBI_instance.retrieve_tfidf(query, k=20)
+        reranked_with_letor = self.letor.rerank_letor(query, [t[1] for t in tf_idf_result])
 
         results_list = []
         for (score, doc) in reranked_with_letor[:k]:
@@ -94,7 +40,9 @@ class SearchLetor:
         elapsed_time = end_time - start_time
         elapsed_time_ms = elapsed_time * 1000
 
-        return {"time": elapsed_time_ms, "results": results_list}
+        elapsed_time_ms_formatted = "{:.3f}".format(elapsed_time_ms)
+
+        return {"time": elapsed_time_ms_formatted, "results": results_list}
 
 
 
